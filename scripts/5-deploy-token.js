63c9fa3e6968a5bdfaa520222c4b9b6337837c9a
MJ LEE <mj1201276@yahoo.com.au>
2022-03-27 20:31:54 +1000

    Added a method to create a governance token for MelonDAO

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,20 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // Deploy a standard ERC-20 contract.
+    const tokenAddress = await sdk.deployer.deployToken({
+      name: "MelonDAO Governance Token",
+      symbol: "MELON",
+      // set to address zero if not selling
+      primary_sale_recipient: AddressZero,
+    });
+    console.log(
+      "✅ Successfully deployed token module, address:",
+      tokenAddress,
+    );
+  } catch (error) {
+    console.error("failed to deploy token module", error);
+  }
+})();
